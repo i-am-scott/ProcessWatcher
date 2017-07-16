467f4e1b834fe0d713b02ced1ff1410010f74821
scottd564 <scottdavie564@msn.com>
2017-07-16 16:58:10 +0100

    webserver init v2

--- APIServer/Routing.cs
+++ APIServer/Routing.cs
@@ -4,15 +4,27 @@ namespace ProcessWatcher.APIServer
 {
     public class Routing : NancyModule
     {
+        // TODO: Don't forget some kind of authentication! (steam somehow?)
         public Routing()
         {
-            Get["/test"] = MainPage;
+            // Display available routes
+            Get["/"] = MainPage;
+
+            // Controls
+            Get["/servers"] = GetServers;
+            Get["/server"] = GetServer;
+            Get["/stats"] = GetStats;
+
+            Post["/server"] = ManageServer;
+            Post["/settings"] = ManageSettings;
         }
 
-        public string MainPage(dynamic parameters)
-        {
-            return "Self hosted webserver!";
-        }
+        public string MainPage(dynamic parameters) => "Not Implemented";
+        public string GetServers(dynamic parameters) => "Not Implemented";
+        public string GetServer(dynamic parameters) => "Not Implemented";
+        public string GetStats(dynamic parameters) => "Not Implemented";
+        public string ManageServer(dynamic parameters) => "Not Implemented";
+        public string ManageSettings(dynamic parameters) => "Not Implemented";
     }
 
 }
